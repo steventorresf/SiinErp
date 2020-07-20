5909fc94dc1ae6c6e5dafadea0b16ee3dc48c390
Steven Torres <steventorresf@gmail.com>
2020-07-20 15:07:50 -0500

    Correccion de tablas

--- SiinErp/wwwroot/master/js/services/services.js
+++ SiinErp/wwwroot/master/js/services/services.js
@@ -841,6 +841,66 @@
 
     angular
         .module('app')
+        .factory('GenTablasEmpresaService', GenTablasEmpresaService);
+
+    GenTablasEmpresaService.$inject = ['$http', '$q'];
+
+    function GenTablasEmpresaService($http, $q) {
+        var nameSpace = '/General/api/TablasEmpresa/';
+
+        var service = {
+            getAll: getAll,
+            create: create,
+            update: update,
+        };
+
+        return service;
+
+        function getAll(idEmp) {
+            return $http.get(nameSpace + idEmp)
+                .then(
+                    function (response) {
+                        return response;
+                    },
+                    function (errResponse) {
+                        console.log(errResponse);
+                        return $q.reject(errResponse);
+                    }
+                );
+        }
+
+        function create(data) {
+            return $http.post(nameSpace, data)
+                .then(
+                    function (response) {
+                        return response;
+                    },
+                    function (errResponse) {
+                        console.log(errResponse);
+                        return $q.reject(errResponse);
+                    }
+                );
+        }
+
+        function update(id, data) {
+            return $http.put(nameSpace + id, data)
+                .then(
+                    function (response) {
+                        return response;
+                    },
+                    function (errResponse) {
+                        console.log(errResponse);
+                        return $q.reject(errResponse);
+                    }
+                );
+        }
+    }
+})();
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
         .factory('GenTablasEmpresaDetService', GenTablasEmpresaDetService);
 
     GenTablasEmpresaDetService.$inject = ['$http', '$q'];
